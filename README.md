a44eb111c12f65afa863721d0e094a0637782ba9
paulopacitti <ppacitti@outlook.com>
2024-03-09 21:15:51 -0300

    First version up and running

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# minecraft-factory
-Deployable Minecraft server with Pulumi
+# pickaxe ⛏️
+Deployable Minecraft server with Pulumi.
+
+![picture of a pickaxe](docs/cover.jpg)
+
+## References
+- https://futurice.com/blog/friends-and-family-minecraft-server-terraform-recipe
+- https://github.com/futurice/terraform-examples/blob/master/google_cloud/minecraft/main.tf
---
+++ index.ts
@@ -0,0 +1,64 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+
+// Config
+const gcpConfig = new pulumi.Config("gcp");
+const zone = gcpConfig.require("zone");
+const projectConfig = new pulumi.Config("minecraft-factory");
+const port = projectConfig.require("port");
+const cpu = projectConfig.require("cpu");
+
+// Network
+const network = new gcp.compute.Network("minecraft-server-network");
+const ip = new gcp.compute.Address("minecraft-server-ip");
+const firewall = new gcp.compute.Firewall("minecraft-server-firewall", {
+  network: network.selfLink,
+  allows: [
+    {
+      protocol: "tcp",
+      ports: [port, "22"],
+    },
+    {
+      protocol: "icmp"
+    }
+  ],
+  sourceRanges: ["0.0.0.0/0"],
+  targetTags: ["minecraft-server"],
+});
+
+// Disk
+const disk = new gcp.compute.Disk("minecraft-server-disk", {
+  size: 10,
+  image: "cos-cloud/cos-stable",
+  type: "pd-standard",
+  zone
+});
+
+// VM instance
+const instance = new gcp.compute.Instance("minecraft-server-instance", {
+  machineType: `n1-standard-${cpu}`,
+  zone: zone,
+  tags: ["minecraft-server"],
+  bootDisk: {
+    autoDelete: false,
+    source: disk.selfLink
+  },
+  metadataStartupScript: `
+    docker run -d -p 25565:25565 -e EULA=TRUE -e VERSION=LATEST \
+      -v /var/minecraft:/data --name mc -e TYPE=PAPER \
+      -e MEMORY=2G --rm=true itzg/minecraft-server:latest;
+  `,
+  networkInterfaces: [{
+    network: network.selfLink,
+    accessConfigs: [{
+      natIp: ip.address,
+    }],
+  }],
+  scheduling: {
+    preemptible: true,
+    automaticRestart: false,
+  },
+});
+
+// Export the IP address
+export const ipAddress = ip.address;
